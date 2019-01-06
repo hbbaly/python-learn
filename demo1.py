5fa39a5bcfb20c9c35bbfec01d1ab4df9246aae4
hbbaly <1422699902@qq.com>
2019-01-06 15:46:56 +0800

    初试python

---
+++ demo1.py
@@ -0,0 +1,19 @@
+# python基本数据类型
+print(type(1))  #int
+print(type(1.234)) #float
+print(type('hbb')) #str
+print(type([]))  #list
+print(type({}))  #dict
+
+print(type(1+1))  #int
+print(type(1+1.1)) #float
+print(type(1+1.0))  #float
+
+
+print(type(1*2))  #int
+print(type(1*2.0)) #float
+# 触发想要得到int，必须用  //
+print(type(4/2))  #float
+print(type(4/2.0))  #float
+print(type(4//2))  #int
+print(type(4//2.0))  #float
