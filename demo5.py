3949a96ce309804db084da1222078803ed52b5c6
hbbaly <1422699902@qq.com>
2019-01-07 22:48:27 +0800

    Update demo5.py

--- demo5.py
+++ demo5.py
@@ -10,5 +10,7 @@ print('C:python\npython-learn')
 
 print('C:python\\npython-learn')
 
-# 也可以使用这种方式,来代替转译字符
+# 也可以使用这种方式,来代替转译字符，字符串前面加上小写r，表示原始字符串，即所见即所得
 print(r'C:python\npython-learn')
+  
+print(r'let's go')   # 这种类型例外，会报错，因为都不是字符串了
