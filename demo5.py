6f33998666a53deed126cc49f9334946170468ae
hbbaly <1422699902@qq.com>
2019-01-07 22:47:26 +0800

    转义字符和原始字符串

---
+++ demo5.py
@@ -0,0 +1,14 @@
+# 转译字符
+# \'   \n   \t
+
+# 输出一个文件夹的路径
+print('C:python\npython-learn')
+# C:python
+# python-learn
+
+# 如果想要输出 'C:python\npython-learn'
+
+print('C:python\\npython-learn')
+
+# 也可以使用这种方式,来代替转译字符
+print(r'C:python\npython-learn')
