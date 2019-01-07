0d9a90f7a683baab467a69765e197bcca78df6e4
hbbaly <1422699902@qq.com>
2019-01-07 23:00:03 +0800

    ADD: 字符串的长度，获取字符串中某个字符

---
+++ demo6.py
@@ -0,0 +1,22 @@
+# 字符串的操作
+
+# len()
+len('hello world')   # 11 长度11
+
+ # 字符串的拼接
+
+'hello ' + 'world'  # 'hello world'
+
+# *  
+
+'hello'*2  # 是把字符串重复2次，'hellohello'
+
+'hello'*0  # ' ' 
+
+# 获取字符串中的某一个字符
+
+'hello world'[0]  # 'h'
+'hello world'[3]  # 'l'
+# [n]表示从字符串下表为0开始到n，n所对应的字符， [-n]表示从字符串的末尾数n次所对应的字符
+'hello world'[-1]  # 'd'
+'hello world'[-2]  # 'l'
