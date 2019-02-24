37109ecffb56d3ec1e6488d0b81cc19a84e47d28
hbbaly <1422699902@qq.com>
2019-02-24 21:20:55 +0800

    ADD：reduce的使用

--- demo12/demo2.py
+++ demo12/demo2.py
@@ -10,4 +10,8 @@ for x in a:
 d = map(square_a, a)
 print(list(d))  # [1, 4, 9, 16, 25, 36, 49, 64]
 
-# map(func, itera)  两个参数，第一个为函数，第二个为 每一项
+# map(func, itera)  两个参数(不一定是两个参数，也可能是更多，这取决于func所需的参数)，第一个为函数，第二个为 每一项
+
+# 使用匿名函数简化上面代码：
+e = map(lambda a: a*a, a)
+print(list(e))
