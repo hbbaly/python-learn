4b55e98dfe75569bc704456b65fbb7abe8f609c7
hbbaly <1422699902@qq.com>
2019-02-26 23:01:56 +0800

    ADD:完善简易爬虫

--- demo13/demo1.py
+++ demo13/demo1.py
@@ -5,10 +5,10 @@ class Spider():
   url = 'https://www.panda.tv/cate/lol?pdt=1.24.s1.3.45k14n9nsbm'
   # video-info 中的所有内容 ()组要匹配video-info中间的内容
   pattern = '<div class="video-info">([\s\S]*?)</div>'
-  pattern_title = '<div class="video-title">([\s\S]*?)</div>'
-  pattern_nickname = '<div class="video-nickname" title="[\s\S]*?">([\s]*?)</div>'
-  pattern_number = '<div class="video-number">([\s]*?)</div>'
-  pattern_num = '<div class="video-station-info"><i class="video-station-num">([\s\S]*?)</i></div>'
+  pattern_title = '<span class="video-title" title="([\s\S]*?)">([\s\S]*?)</span>'
+  pattern_nickname = '<span class="video-nickname" title="([\s\S]*?)">'
+  pattern_number = '<span class="video-number"><i class="ricon ricon-eye"></i>([\s\S]*?)</span>'
+  pattern_num = '<i class="video-station-num">([\s\S]*?)</i>'
   def __get_data(self):
     data = request.urlopen(self.url)
     html = data.read()
@@ -16,16 +16,34 @@ class Spider():
     return html_str
   def start(self):
     htmlstr = self.__get_data()
-    self.analysis(htmlstr)
+    htmlstr = self.analysis(htmlstr)
+    htmlstr = self.__sorted(htmlstr)
+    self.__print(htmlstr)
+    # print(htmlstr['name']+'-----------'+htmlstr['number'])
+
+  def __sorted(self, anchor):
+    anchor = sorted(anchor, key=self.__sorted__seed, reverse=True)
+    return anchor
+
+  def __sorted__seed(self, anchor):
+    r = re.findall('\d*',anchor['number'])
+    r = float(r[0])
+    if '万' in anchor['number']:
+      r *=10000
+    return r
+  def __print(self, anchors):
+    for rank in range(0, len(anchors)):
+      print('rank'+':'+str(rank+1)+'---------'+anchors[rank]['name']+'------'+anchors[rank]['number'])
+
   def analysis(self, html):
     html_all = re.findall(self.pattern, html)
     data_list = []
-
+  
     for html in html_all:
-      name = re.findall(self.pattern_nickname, html)
-      title = re.findall(self.pattern_title, html)
-      number = re.findall(self.pattern_number, html)
-      num = re.findall(self.pattern_num, html)
+      name = re.findall(self.pattern_nickname, html)[0]
+      title = re.findall(self.pattern_title, html)[0][1]
+      number = re.findall(self.pattern_number, html)[0]
+      num = re.findall(self.pattern_num, html)[0]
       all_data = {
         "name":name,
         "title":title,
@@ -33,7 +51,7 @@ class Spider():
         "num":num
       }
       data_list.append(all_data)
-    print(data_list)
+    return data_list
 
 
 spider = Spider()
