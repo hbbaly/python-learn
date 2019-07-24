555a24ad33228111cfa64c899b137f59a8dcab07
hbbaly <1422699902@qq.com>
2019-07-24 09:28:12 +0800

    ADD: python3 链接数据库及基础操作

---
+++ demo1/1.py
@@ -0,0 +1,18 @@
+# 使用pymysql链接mysql
+import pymysql
+
+# 打开数据库连接  ip 用户名 密码 数据库
+db = pymysql.connect("localhost","root","123456789","test" )
+ 
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+# 使用 execute() 方法执行 SQL，如果表存在则删除
+# cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+cursor.execute("""SELECT * FROM `EMPLOYEE`""")
+# 使用 fetchone() 方法获取单条数据.
+data = cursor.fetchone()
+ 
+print (data)
+ 
+# 关闭数据库连接
+db.close()
