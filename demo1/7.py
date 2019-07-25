f85043b4757f4d784055d5eba1debd352cabc705
hbbaly <1422699902@qq.com>
2019-07-25 13:55:38 +0800

    ADD：面向对象编程，并把获取到的转为字典

--- demo1/7.py
+++ demo1/7.py
@@ -9,22 +9,39 @@ class DB() :
   # 返回游标
   def __enter__(self):
     return self.cursor
-
+  def close_connect(self):
+    self.connect.close()
   # 
   def createDb(self, sql):
-    self.__isExsist__()
+    self.__isExsistDb__()
     self.cursor.execute(sql)
-    self.connect.commit()
-    self.connect.close()
+    self.close_connect()
 
-  def __isExsist__(self):
+  def __isExsistDb__(self):
     self.cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
 
+  def findData(self, sql, type="all"):
+    self.cursor.execute(sql)
+    data = ''
+    if type == "all":
+      data = self.cursor.fetchall()
+    else:
+      data = self.cursor.fetchone()
+    self.connect.commit()
+    self.close_connect()
+    # data是一个元祖
+    data = [dict(zip([item[0] for item in self.cursor.description], data))]
+    return data
 sql = """CREATE TABLE EMPLOYEE (
   FIRST_NAME  CHAR(20) NOT NULL,
   LAST_NAME  CHAR(20),
   AGE INT,  
   SEX CHAR(1),
   INCOME FLOAT )"""
-test = DB().createDb(sql)
-print(test)
+test = DB()
+# print(test.createDb(sql))
+
+findOneSql = "SELECT * FROM `test`.`user`"
+
+print(test.findData(findOneSql, 'one')[0])
+
