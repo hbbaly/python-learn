50f4ef52fe5ff92b2e30d44d142f5263735bead7
hbbaly <1422699902@qq.com>
2019-07-26 13:58:37 +0800

    ADD:面向对象基本操作

--- demo1/7.py
+++ demo1/7.py
@@ -35,6 +35,18 @@ class DB() :
     # data是一个元祖
     
     return data
+  def insert_data(self, sql):
+    self.cursor.execute(sql)
+    self.connect.commit()
+    self.close_connect()
+  def update_data(self, sql):
+    self.cursor.execute(sql)
+    self.connect.commit()
+    self.close_connect()
+  def delete_data(self, sql):
+    self.cursor.execute(sql)
+    self.connect.commit()
+    self.close_connect()
 sql = """CREATE TABLE EMPLOYEE (
   FIRST_NAME  CHAR(20) NOT NULL,
   LAST_NAME  CHAR(20),
@@ -45,6 +57,14 @@ test = DB()
 # print(test.createDb(sql))
 
 findOneSql = "SELECT * FROM `test`.`user`"
+update_sql = """UPDATE `EMPLOYEE` SET AGE = AGE + 18"""
+insert_sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Macb', 'Mo', 20, 'M', 20000)"""
+delete_sql = """DELETE FROM `EMPLOYEE` WHERE AGE > 30"""
+# print(test.findData(findOneSql, 'one'))
+# print(test.update_data(update_sql))
+# print(test.insert_data(insert_sql))
+print(test.delete_data(delete_sql))
 
-print(test.findData(findOneSql, 'one'))
-
+# 增删改查
